b9361e7bf1e5f37f7a691b39fea31e0f14e69669
Vadym Chepkov <vchepkov@gmail.com>
2016-01-24 07:00:21 -0500

    added parameters to main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,19 @@ class s3cmd (
   $aws_secret_key,
   $gpg_passphrase,
   $owner,
+  $enable_repo = true,
+  $version     = 'installed',
 ) {
-  class{'s3cmd::install':}
+
+  class{'s3cmd::install':
+    enable_repo => $enable_repo,
+    version     => $version,
+  }
   class{'s3cmd::config':
-      aws_access_key => $aws_access_key,
-      aws_secret_key => $aws_secret_key,
-      gpg_passphrase => $gpg_passphrase,
-      owner          => $owner,
-      require        => Class['s3cmd::install'],
+    aws_access_key => $aws_access_key,
+    aws_secret_key => $aws_secret_key,
+    gpg_passphrase => $gpg_passphrase,
+    owner          => $owner,
+    require        => Class['s3cmd::install'],
   }
 }
