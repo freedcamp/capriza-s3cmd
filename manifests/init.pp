dc49b1768d0aeb0347b764eda0954d9ec3472e6d
Vadym Chepkov <vchepkov@gmail.com>
2017-11-16 08:31:00 -0500

    change s3cmd::config to a define to allow multiple users configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,20 +2,20 @@ class s3cmd (
   $aws_access_key,
   $aws_secret_key,
   $gpg_passphrase,
-  $owner,
+  $owner       = 'root',
   $enable_repo = true,
   $version     = 'installed',
 ) {
 
-  class{'s3cmd::install':
+  class { 's3cmd::install':
     enable_repo => $enable_repo,
     version     => $version,
   }
-  class{'s3cmd::config':
+
+  s3cmd::config { $owner:
     aws_access_key => $aws_access_key,
     aws_secret_key => $aws_secret_key,
     gpg_passphrase => $gpg_passphrase,
-    owner          => $owner,
     require        => Class['s3cmd::install'],
   }
 }
